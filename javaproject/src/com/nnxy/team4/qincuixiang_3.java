ebbd88f5fc9ea6fd4e98d3a1237c590fb140a14d
qincuixiang <1471116495@qq.com>
2020-12-11 17:44:25 +0800

    5-22提交

--- javaproject/src/com/nnxy/team4/qincuixiang_3.java
+++ javaproject/src/com/nnxy/team4/qincuixiang_3.java
@@ -5,11 +5,9 @@ public class qincuixiang_3 {
   public int id;
   
   private int gitId() {
-<<<<<<< HEAD
-	  return id + 59;
-=======
-	  return id +59;
->>>>>>> branch 'master' of https://github.com/qincuixiang/java.pro.git
+
+	  return id + 20;
+
   }
   public void setId(int id) {
 	this.id = id;
