af5f57939ade7d7ace7eda08a15f107eb53cc34d
qincuixiang <1471116495@qq.com>
2020-12-11 11:16:50 +0800

    5-11提交

--- javaproject/src/com/nnxy/team4/qincuixiang_3.java
+++ javaproject/src/com/nnxy/team4/qincuixiang_3.java
@@ -1,5 +1,15 @@
 package com.nnxy.team4;
 
 public class qincuixiang_3 {
-
-}
+	
+  public int id;
+  
+  private int gitId() {
+	  return id;
+  }
+  public void setId(int id) {
+	this.id = id;
+	
+  }
+  
+  }
